1c0bf4304eea69e5c19627a88658ce738ef8a480
CataL999 <71272485+CataL999@users.noreply.github.com>
2023-06-15 14:02:04 +0300

    Update settings.py
    Signed-off-by: CataL999 <71272485+CataL999@users.noreply.github.com>

--- ecommerce_project/ecommerce_project/settings.py
+++ ecommerce_project/ecommerce_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a1w&)9enng-xi-p2qdpzotvzz9dadu@(w-aj(lwv2w_=d2kgx+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
